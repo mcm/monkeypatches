49cbb90355dd54cbd0afbe24d130a9ddb1fd791d
steve <steve@mcmaster.io>
2025-08-12 03:40:21 +0000

    Remove unused imports [CI SKIP]

--- src/main/java/io/mcmaster/monkeypatches/mixin/jeresources/GH547/BiomeHelperMixin.java
+++ src/main/java/io/mcmaster/monkeypatches/mixin/jeresources/GH547/BiomeHelperMixin.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 
 import io.mcmaster.monkeypatches.util.MixinConditions;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
